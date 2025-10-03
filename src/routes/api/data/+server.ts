b339da9f8434e67131d54a1a1fe966aff798af05
iliksis <40835286+iliksis@users.noreply.github.com.>
2025-10-03 12:01:00 +0200

    Removed test data

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -1,14 +1,9 @@
 import { getOverlayStates, getTmpData } from '$lib/server/state.svelte';
 import { json } from '@sveltejs/kit';
-import { USE_TEST_DATA } from '$env/static/private';
 
-export const GET = async ({ fetch }) => {
-	let data = getTmpData();
+export const GET = async () => {
+	const data = getTmpData();
 	const overlayState = getOverlayStates();
-	if (USE_TEST_DATA) {
-		const res = await fetch('/testData.json');
-		data = (await res.json()).data;
-	}
 
 	return json({
 		data,
