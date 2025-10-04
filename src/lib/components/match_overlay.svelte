cc7c535e43dd6d2b10289bb0170a37d6358e6342
iliksis <40835286+iliksis@users.noreply.github.com.>
2025-10-04 15:57:01 +0200

    Updated styling

--- src/lib/components/match_overlay.svelte
+++ src/lib/components/match_overlay.svelte
@@ -10,21 +10,21 @@
 	export let status: State;
 </script>
 
-<div class="grid w-full grid-cols-3 items-center text-2xl">
-	<div class="flex items-center justify-start">
+<div class={['grid w-full grid-cols-[1fr_auto_1fr] gap-10 text-2xl', sets.length === 0 && 'mb-7']}>
+	<div class="flex justify-start">
 		<div class="text-left">
 			<div class="font-semibold">{homePlayer}</div>
 		</div>
 	</div>
-	<div class={['text-center font-bold opacity-60', status === 'ONGOING' && 'opacity-100']}>
-		{score}
+	<div class="text-center">
+		<div class={['font-bold opacity-60', status === 'ONGOING' && 'opacity-100']}>{score}</div>
+		<div class="text-xl opacity-60">
+			{sets}
+		</div>
 	</div>
-	<div class="flex items-center justify-end">
+	<div class="flex justify-end">
 		<div class="text-right">
 			<div class="font-semibold">{awayPlayer}</div>
 		</div>
 	</div>
-</div>
-<div class="text-xl opacity-70">
-	{sets}
 </div>
--- src/routes/overlay/table/[tableId]/+page.svelte
+++ src/routes/overlay/table/[tableId]/+page.svelte
@@ -98,7 +98,7 @@
 {:else if overlayState === 'overview'}
 	{#if data}
 		<div
-			class="absolute top-0 right-0 bottom-0 left-0 m-auto h-fit w-5xl items-start justify-center rounded-md bg-linear-to-br from-player/80 to-match/80 p-6 text-white"
+			class="absolute top-0 right-0 bottom-0 left-0 m-auto h-fit w-5xl items-start justify-center rounded-md bg-linear-to-br from-player/80 to-match/80 p-4 text-white"
 		>
 			<!-- Team Header -->
 			<div class="m-2 mt-2 mb-8 grid grid-cols-3">
